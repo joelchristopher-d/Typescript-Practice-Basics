14c2ed396de6a116f3098488e7ba089614fa37c0
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-02-06 22:00:57 +0530

    Add files via upload

---
+++ datatypes.ts
@@ -0,0 +1,16 @@
+let mynum:number=1;
+let myname:string;
+let role:string;
+let age:number=24;
+let price:number|string="77";
+let value:boolean=true;
+let some:any;
+
+let re:RegExp=/\w+/g;
+
+some=99.99;
+myname="joel";
+role="dev";
+const sum=(a:number,b:string)=>{
+    return a+b;
+}

c98dcd25a233e54085ce9011f653cd285ecceaf5
Joel Christopher <118204275+joelchristopher-d@users.noreply.github.com>
2024-02-13 21:03:48 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+let uname = "joel";
+console.log(uname);
+
+
+
+// unlike js, Ts gives warning for different datatype
+let a = 12;
+let b = '6';
+//console.log(a/b);
+
+let c:number=22;
+let d:number=11;
+let e:string="joel";
+console.log(c/d);
